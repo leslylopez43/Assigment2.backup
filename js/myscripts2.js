b1295a440d98617a260070c51ca36bbeed6a10bd
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-06-25 14:33:43 +0100

    update function

--- js/myscripts2.js
+++ js/myscripts2.js
@@ -1,42 +1,41 @@
-var difficult="";
+var difficult = "";
 
-function startgame()
-{
+function startgame() {
   //let onscreenBoard=getElementsByClassName(startgame);
-// the text box identifie by Player name
- Pname=document.getElementById("PlayerName").value;
- localStorage.setItem("PN",Pname)
+  // the text box identifie by Player name
+  Pname = document.getElementById("PlayerName").value;
+  localStorage.setItem("PN", Pname);
   //the information store is identify the Pn Name
-  localStorage.setItem("dn",difficult)
-   if(difficult=="" || Pname=="")
-   {
-  // confirm("You need to choose hard or easy and also enter a name")
-  document.getElementById("chooseAName").innerHTML="!Type a Name then choose Hard or Easy";
-   }
-   else
-   {
-  window.location.href="html/home.html"; //is change for the location of the page
-   }
+  localStorage.setItem("dn", difficult);
+  if (difficult == "" || Pname == "") {
+    // confirm("You need to choose hard or easy and also enter a name")
+    document.getElementById("chooseAName").innerHTML =
+      "!Type a Name then choose Hard or Easy";
+  } else {
+    window.location.href = "html/home.html"; //is change for the location of the page
+  }
 }
 
-function detectKey(e)
-{
-    if (e.key == "Enter")
-  startgame();
-   }
+function detectKey(e) {
+  if (e.key == "Enter") {
+    startgame();
+  }
+}
 
- function easy()
- {
+function easy() {
   difficult = "easy-mode";
   // alert("difficult = " + difficult);
-  startgame();
-  document.getElementById("game-container").classList.add("easy-mode");
+  {
+    startgame();
+    document.getElementById("game-container").classList.add("easy-mode");
+  }
 }
 
-function hard()
-{
-  difficult= "hard-mode";
+function hard() {
+  difficult = "hard-mode";
   // alert("hard")
-  startgame();
+  {
+    startgame();
+  }
   document.getElementById("game-container").classList.add("hard-mode");
 }
